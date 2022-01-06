52b9993eff90cdad71334a304fd040f231dc800a
JKrecisz <jakub.krecisz@icloud.com>
2022-01-06 03:34:18 +0100

    Created .py files for classes, added images of aliens and ship

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import pygame
+import settings
 
 # Initializing pygame
 pygame.init()
-running = True
 pygame.mouse.set_visible(False)
+clock = pygame.time.Clock()
 
 # Screen setup
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-pygame.display.set_caption('Space-invaders by @JKrecisz')
-background = pygame.image.load('images/background.png')
-pygame.display.set_icon(pygame.image.load('images/icon.png'))
+pygame.display.set_caption(settings.caption)
+background = pygame.image.load(settings.background_image)
+pygame.display.set_icon(pygame.image.load(settings.icon_image))
 
+
+# TODO Sounds setup
+"""
+explosion_fx = pygame.mixer.Sound("img/explosion.wav")
+explosion_fx.set_volume(0.25)
+
+explosion2_fx = pygame.mixer.Sound("img/explosion2.wav")
+explosion2_fx.set_volume(0.25)
+
+laser_fx = pygame.mixer.Sound("img/laser.wav")
+laser_fx.set_volume(0.25)
+"""
+
+
+running = True
 while running:
     screen.blit(background, (0, 0))
+    clock.tick(settings.fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     pygame.display.update()
+
+    
+
+pygame.quit()
---
+++ settings.py
@@ -0,0 +1,23 @@
+# Icon
+icon_image = 'images/icon.png'
+
+# Background
+background_image = 'images/background.png'
+
+# Ship
+ship_image = 'images/ship.png'
+
+# Alien
+alien_image = ['images/alien1.png',
+               'images/alien2.png',
+               'images/alien3.png',
+               'images/alien4.png',
+               'images/alien5.png']
+
+
+# Caption
+caption = 'Space-invaders by @JKrecisz'
+
+# frames per second (FPS)
+fps = 60
+
