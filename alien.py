4b899cf6b24f2956352bb09c3bc103d5afa9cb75
Jakub Kręcisz <jakub.krecisz@icloud.com>
2022-01-07 05:12:46 +0100

    Delete alien.py

--- alien.py
+++
@@ -1,9 +0,0 @@
-import pygame.image
-from settings import alien_image
-import random
-
-
-class Aliens(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(alien_image[random.randint(0, 4)])
